9c20059e19ed202fb41f5597df4b5629512187bf
Benjamin Birnbaum <132301476+ThugLyfe02@users.noreply.github.com>
2026-03-08 10:34:14 -0400

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -37,6 +37,7 @@ from app.api.fraud_attacks import router as fraud_attack_router
 from app.api.fraud_soc import router as fraud_soc_router
 from app.api.fraud_platform import router as fraud_platform_router
 from app.api.fraud_graph_visualization import router as fraud_graph_visualization_router
+from app.subscribers import register_all_subscribers
 
 
 from app.database import engine
@@ -88,6 +89,7 @@ app.include_router(fraud_attack_router)
 app.include_router(fraud_soc_router)
 app.include_router(fraud_platform_router)
 app.include_router(fraud_graph_visualization_router)
+register_all_subscribers()
 
 
 @app.get("/")
